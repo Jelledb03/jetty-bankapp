26707c7ada8b77f3474f1796f10482f37c643472
Jelle De Boeck <Jelle.DeBoeck@gmail.com>
2020-03-29 19:24:00 +0200

    First Commit

--- src/main/java/BankMain.java
+++ src/main/java/BankMain.java
@@ -2,7 +2,7 @@
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
-public class EmbeddedJettyMain {
+public class BankMain {
 
     public static void main(String[] args) throws Exception {
 
